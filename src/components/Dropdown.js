d5e058ebfdd38b528d0a40d199e3454bf45041c5
Juhye O <artigen3@gmail.com>
2024-02-02 17:20:58 +0900

    Dropdown with css

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,6 @@
 import { useEffect, useRef, useState } from "react";
+import { FiMenu } from "react-icons/fi";
+import { GoChevronRight } from "react-icons/go";
 
 function Dropdown({options, value, onChange}){
     const [isOpen, setIsOpen] = useState(false);
@@ -34,18 +36,24 @@ function Dropdown({options, value, onChange}){
     const renderedOptions = options.map((option, index) => {
         return (
             <div key={index} 
-                className="flex items-center" 
+                className="cursor-pointer flex justify-between items-center text-main-blue text-xl border-b-2 border-main-blue py-3.5" 
                 onClick={() => {handleOptionClick(option)}}>
+                <div className="flex items-center size-full">
                 {option.label}
+                <span className="ml-1">
                 {option.icon}
+                </span>
+                </div>
+                <div><GoChevronRight /></div>
             </div>
         );
     });
 
     return (
         <div ref={divEl}>
-            <div onClick={handleClick}>
-            {value?.label || 'Select your SNS'} 
+            <div className="w-28 bg-main-blue rounded text-white p-2 text-lg text-center" onClick={handleClick}>
+            {value?.label || 
+            <div className="flex items-center justify-around"><FiMenu /> MENU</div>} 
             </div>
             {isOpen && renderedOptions}
         </div>
@@ -53,3 +61,4 @@ function Dropdown({options, value, onChange}){
 }
 
 export default Dropdown;
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'main-blue': '#0062F5',
+      },
+    },
   },
   plugins: [],
 }
